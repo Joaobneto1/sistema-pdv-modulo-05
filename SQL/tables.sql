7afebb9e7d5d290729ff29efb393d4bf9ea40c3f
unknown <mateusjsp.eth@gmail.com>
2024-01-17 19:07:20 -0300

    tabela

---
+++ SQL/tables.sql
@@ -0,0 +1,32 @@
+CREATE TABLE categorias (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE clientes (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100),
+    telefone VARCHAR(20)
+);
+
+CREATE TABLE pedidos (
+    id SERIAL PRIMARY KEY,
+    cliente_id INTEGER REFERENCES clientes(id),
+    data_pedido DATE NOT NULL,
+    total_centavos INTEGER NOT NULL
+);
+
+CREATE TABLE produtos (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    preco_centavos INTEGER NOT NULL,
+    categoria_id INTEGER REFERENCES categorias(id)
+);
+
+CREATE TABLE usuarios (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    senha VARCHAR(100) NOT NULL
+);
