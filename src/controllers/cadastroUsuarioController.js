ec6771831c26ad28aea4a70bc1d9c780f5cd8079
Leonardo Guiato <123307371+LeoguiatoM5@users.noreply.github.com>
2024-01-19 12:05:57 -0300

    Update cadastroUsuarioController.js
    Signed-off-by: Leonardo Guiato <123307371+LeoguiatoM5@users.noreply.github.com>

--- src/controllers/cadastroUsuarioController.js
+++ src/controllers/cadastroUsuarioController.js
@@ -37,36 +37,6 @@ const cadastrarUsuario = async (req, res) => {
 };
 
 
-/*const cadastrarUsuario = async (req, res) => {
-  const cadastrarUsuario = async (req, res) => {
-  const { nome, email, senha } = req.body;
-
- 
-  if (!nome || !email || !senha) {
-    return res.status(400).json({ erro: 'Todos os campos devem ser preenchidos' });
-  }
-
-  try {
-    const usuarioExistente = await conexao('usuarios').where('email', email).first();
-    if (usuarioExistente) {
-      return res.status(400).json({ erro: 'E-mail já cadastrado' });
-    }
-
-    const senhaHash = await bcrypt.hash(senha, 10);
-
-    const usuarioCadastrado = await conexao('usuarios').insert({
-      nome,
-      email,
-      senha: senhaHash,
-    }).returning(['nome', 'email']);  
-
-    return res.status(201).json({ mensagem: 'Usuário cadastrado com sucesso', usuario: usuarioCadastrado[0] });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ erro: 'Erro interno do servidor' });
-  }
-}*/
-
 
 
 
