137e5770b8fab97599821eee2f3fcda67db41580
unknown <mateusjsp.eth@gmail.com>
2024-02-08 18:05:16 -0300

    update

--- src/controllers/controllersProduct/editarProduto.js
+++ src/controllers/controllersProduct/editarProduto.js
@@ -4,15 +4,13 @@ const { schemaEditarProduto } = require('../../validations/validacoesEditarProdu
 
 const editarProduto = async (req, res) => {
     const IdProduto = req.params.id;
-
     const { error, value } = schemaEditarProduto.validate(req.body);
 
     if (error) {
         return res.status(400).json({ mensagem: error.message });
     }
 
-    const { descricao, quantidade_estoque, valor, categoria_id, produto_imagem } = value;
-    const { originalname, mimetype, buffer } = req.file
+    const { descricao, quantidade_estoque, valor, categoria_id } = value;
 
     try {
         const categoriaExistente = await knex("categorias")
@@ -22,6 +20,7 @@ const editarProduto = async (req, res) => {
         if (!categoriaExistente) {
             return res.status(404).json({ mensagem: 'A categoria informada não existe.' });
         }
+
         let produtoEditado = await knex('produtos')
             .where({ id: IdProduto })
             .update({
@@ -29,29 +28,27 @@ const editarProduto = async (req, res) => {
                 quantidade_estoque,
                 valor,
                 categoria_id,
-
             });
 
         if (!produtoEditado) {
             return res.status(404).json({ mensagem: 'Produto não existe' });
         }
 
-        await excluirImagem(produtoEditado.produto_imagem)
-        const upload = await uploadImagem(
-            `produtos/${produtoEditado.id}/${originalname}`,
-            buffer,
-            mimetype
-        )
-        produtoEditado = await knex('produtos')
-            .where({ id: IdProduto })
-            .update({
-                produto_imagem: upload.path
-            })
+        if (req.file) {
+            const { originalname, mimetype, buffer } = req.file;
+            await excluirImagem(produtoEditado.produto_imagem);
+            const upload = await uploadImagem(`produtos/${IdProduto}/${originalname}`, buffer, mimetype);
 
+            produtoEditado = await knex('produtos')
+                .where({ id: IdProduto })
+                .update({
+                    produto_imagem: upload.path
+                }, ['produto_imagem']);
+        }
 
-        return res.status(200).json({ mensagem: 'Produto atualizado com sucesso.', produto: produtoEditado[0] });
+        return res.status(200).json({ mensagem: 'Produto atualizado com sucesso.', produto: produtoEditado });
     } catch (error) {
-        res.status(500).json({ mensagem: 'Erro interno do servidor' });
+        return res.status(500).json({ mensagem: 'Erro interno do servidor' });
     }
 };
 
